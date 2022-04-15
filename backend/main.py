d9ba9fde62c838a66bd78933b22ef985685ff475
Asper <gmunozrodr@gmail.com>
2022-04-15 15:47:45 +0200

    Si alguien consigue que esto funcione en linux, este cambio será de ayuda

--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,10 @@ import sys
 if __name__ == "__main__":
     arg = sys.argv[1] if len(sys.argv) > 1 else None
     if arg != "bot":
-        print("Initializing Bot")
-        botThr = Thread(target=runBot, daemon=True)
+        print("Initializing Server")
+        botThr = Thread(target=runFlask, daemon=True, args=[arg])
         botThr.start()
-        print("initializing Server")
-        runFlask(arg)
+        print("initializing Bot")
+        runBot()
     else:
         runBot(standalone=True)
